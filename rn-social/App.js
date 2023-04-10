f106815c6455f2b62943345c2e2a6ca030c83980
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-04-10 19:40:22 +0300

    commit_add-upd

--- rn-social/App.js
+++ rn-social/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
+import { Provider } from "react-redux";
+import {store} from "./redux/store"
 
 
 export default function App() {
   const routing = useRoute(true);
 
   return (
-    <NavigationContainer>
+    <Provider store ={store}>
+      <NavigationContainer>
       {routing}
-    </NavigationContainer>  
+      </NavigationContainer>  
+    </Provider>
   );
 }
